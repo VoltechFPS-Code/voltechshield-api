3a4cc373282f2b310d97133b4650f7b9f88963d8
VoltechFPS-Code <logins@voltechfps.com>
2026-03-19 20:24:56 +0400

    Update server.js v1.0.1

--- server.js
+++ server.js
@@ -234,9 +234,9 @@ app.get("/health", (_req, res) => {
 
 app.get("/version", (_req, res) => {
   return res.json({
-    version: "1.0.0",
-    notes: "First Public Release",
-    url: "https://github.com/VoltechFPS-Code/voltechshield-api/releases/download/v1.0.0/VoltechShield_1.0.0_x64-setup.exe"
+    version: "1.0.1",
+    notes: "Patches for Hotkey, CMD Terminal, Minor Bug Fixes",
+    url: "https://github.com/VoltechFPS-Code/voltechshield-api/releases/download/v1.0.1/VoltechShield_1.0.1_x64-setup.exe"
   });
 });
 
